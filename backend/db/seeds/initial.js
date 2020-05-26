98d09bb2c13ead87ed77717662fa4145cc1daea6
Andy Herzog <asherzog19@gmail.com>
2020-05-25 18:00:08 -0600

    updated seeds

--- backend/db/seeds/initial.js
+++ backend/db/seeds/initial.js
@@ -20,31 +20,48 @@ exports.seed = async (knex) => {
   await orderedTables
     .reduce(async (promise, name) => {
       await promise
+      // restart all sequences with the fresh seeds
       await knex(name).del()
       return knex.raw(`ALTER SEQUENCE ${name}_id_seq RESTART WITH 1`) 
     }, Promise.resolve())
-
+  
+  // create first user
   const password = crypto.randomBytes(15).toString('hex')
-
   const user = {
     email: 'andy@null.computer',
     name: 'Andy',
     password : await bcrypt.hash(password, 12)
   }
-
   const [createdUser] = await knex(tableNames.user).insert(user).returning('*')
-
   console.log(`user created: `, {
     password}, createdUser)
   
+  // create all the teams
   await knex(tableNames.team).insert(teams).returning('*')
-
+  
+  // create first game
   const game = { 
     home_id: 12,
-    away_id: 15,
+    away_id: 27,
     game_time: new Date(),
     season_week: '20-5'
   }
-
   await knex(tableNames.game).insert(game).returning('*')
-};
+
+  // create first league
+  const league = {name: "test league"}
+  const user_league = {user_id: 1, league_id: 1}
+  await knex(tableNames.league).insert(league).returning('*')
+  await knex(tableNames.user_league).insert(user_league).returning('*')
+
+  // create first pick
+  const pick = {
+    league_id: 1,
+    user_id: 1,
+    game_id: 1,
+    team_id: 12,
+    home_score: 27,
+    away_score: 14
+  }
+  await knex(tableNames.pick).insert(pick).returning('*')
+}
